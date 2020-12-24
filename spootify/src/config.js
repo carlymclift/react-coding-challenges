cdea6796431b799209e2864c3d3763b73009c464
Carly Clift <carlymclift@gmail.com>
2020-12-23 22:52:00 -0700

    Update with correct clientid and secret

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'carlyc',
-    clientSecret: 'helloWorld1'
+    clientId: '37e6d2af81df4b5cab543002daaac469',
+    clientSecret: 'cf80db54323348538a896bcd66f43e7c'
   }
 }
